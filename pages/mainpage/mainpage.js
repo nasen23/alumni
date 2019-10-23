f9c43b1fdedbe9da7ba3ea6f41889b5a5b71fbd1
Sen Na <1439468790@qq.com>
2019-10-23 20:37:45 +0800

    feat: navigate to activity detail using id

--- pages/mainpage/mainpage.js
+++ pages/mainpage/mainpage.js
@@ -3,11 +3,11 @@ Page({
     historyActivities: ["历史1", "历史2"],
     activityTypes: ["全部", "类型1", "类型2"],
     activities: [
-      { "name": "活动名称", "intro": "活动简介", "tags": ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"], "activityTime": "2019年10月14日", "activityLocation": "清华大学桃李园地下餐厅", "activityObject": ["xxxx级本科/研究生", "xx专业/院系","年龄段"],},
-      { "name": "活动名称", "intro": "活动简介", "tags": ["1"] },
-      { "name": "活动名称", "intro": "活动简介", "tags": ["1"] },
-      { "name": "活动名称", "intro": "活动简介", "tags": ["1"] },
-      { "name": "活动名称", "intro": "活动简介", "tags": ["1"] }
+      { "id": 1, "name": "活动名称", "intro": "活动简介", "tags": ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"], "activityTime": "2019年10月14日", "activityLocation": "清华大学桃李园地下餐厅", "activityObject": ["xxxx级本科/研究生", "xx专业/院系","年龄段"],},
+      { "id": 2, "name": "活动名称", "intro": "活动简介", "tags": ["1"] },
+      { "id": 3, "name": "活动名称", "intro": "活动简介", "tags": ["1"] },
+      { "id": 4, "name": "活动名称", "intro": "活动简介", "tags": ["1"] },
+      { "id": 5, "name": "活动名称", "intro": "活动简介", "tags": ["1"] }
     ]
   },
   inputTyping: function () {
@@ -16,7 +16,7 @@ Page({
   },
   toActivityDetail: function (e) {
     wx.navigateTo({
-      url: "/pages/activity-details/index?id=" + e.currentTarget.dataset.id
+      url: "/pages/activity/tmpactivity/tmpactivity?id=" + e.currentTarget.dataset.id
     })
   }
 })
