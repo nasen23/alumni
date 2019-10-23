e60603b316eb223b56b7b39613f0f96051314f8b
Sen Na <1439468790@qq.com>
2019-10-23 18:50:54 +0800

    feat: search bar and mini history view

--- pages/mainpage/mainpage.js
+++ pages/mainpage/mainpage.js
@@ -1,14 +1,14 @@
 Page({
   data: {
-    history: ["类型1", "类型2", "类型3", "类型4", "类型5"],
-    activityTypes: ["全部","类型1", "类型2", "类型3", "类型4", "类型5"],
-    activity: [
-      {
-        "activityName": "活动名称", "activityIntro": "活动简介", "activityTag": ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"], "activityTime": "2019年10月14日", "activityLocation": "清华大学桃李园地下餐厅", "activityObject": ["xxxx级本科/研究生", "xx专业/院系","年龄段"],}, 
-      { "activityName": "活动名称", "activityIntro": "活动简介", "activityTag": ["1"] }, 
-      { "activityName": "活动名称", "activityIntro": "活动简介", "activityTag": ["1"] }, 
-      { "activityName": "活动名称", "activityIntro": "活动简介", "activityTag": ["1"] }, 
-      { "activityName": "活动名称", "activityIntro": "活动简介", "activityTag": ["1"] }]},
+    historyActivities: ["历史1", "历史2"],
+    activityTypes: ["全部", "类型1", "类型2"],
+    activities: [
+      { "name": "活动名称", "intro": "活动简介", "tags": ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"], "activityTime": "2019年10月14日", "activityLocation": "清华大学桃李园地下餐厅", "activityObject": ["xxxx级本科/研究生", "xx专业/院系","年龄段"],},
+      { "name": "活动名称", "intro": "活动简介", "tags": ["1"] },
+      { "name": "活动名称", "intro": "活动简介", "tags": ["1"] },
+      { "name": "活动名称", "intro": "活动简介", "tags": ["1"] },
+      { "name": "活动名称", "intro": "活动简介", "tags": ["1"] }]
+  },
   inputTyping: function () {
     var obj = JSON.stringify(this.data.history);
     wx.navigateTo({url:"./searchpage/search?history=" + obj});
