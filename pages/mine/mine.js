6dec35f47afb84d84ecdee1d6b700ad7a35246f7
QiuYuanhui <qiuyh0924@163.com>
2019-10-24 22:28:28 +0800

    chore: add personal profile page

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -1,4 +1,7 @@
 // pages/mine/mine.js
+const app = getApp()
+const CONFIG = require('../../config.js')
+
 Page({
 
   /**
@@ -25,8 +28,22 @@ Page({
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
+  onShow() {
+    const _this = this
+    this.setData({
+      version: CONFIG.version,
+      userInfo: "",
+      // vipLevel: app.globalData.vipLevel
+    })
+    // AUTH.checkHasLogined().then(isLogined => {
+    //   if (isLogined) {
+    //     _this.setData({
+    //       userInfo: wx.getStorageSync('userInfo')
+    //     })
+    //     _this.getUserApiInfo();
+    //     _this.getUserAmount();
+    //   }
+    // })
   },
 
   /**
@@ -62,5 +79,41 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  onGotUserInfo(e) {
+    // if (!e.detail.userInfo) {
+    //   wx.showToast({
+    //     title: '您已取消登录',
+    //     icon: 'none'
+    //   })
+    //   return;
+    // }
+    // if (app.globalData.isConnected) {
+    //   wx.setStorageSync('userInfo', e.detail.userInfo)
+    //   AUTH.login(this);
+    // } else {
+    //   wx.showToast({
+    //     title: '当前无网络',
+    //     icon: 'none'
+    //   })
+    // }
+  },
+
+  about: function () {
+      wx.showModal({
+          title: '关于',
+          content: '清友活动圈版本0.0.1\n开发人员：\n杨松霖\n那森\n邱圆辉\n金叶\n郭勇毅',
+      })
+  },
+
+  logout: function () {
+      wx.showModal({
+          title: '提示',
+          content: '是否确定注销当前帐号？',
+          success: function () {
+
+          }
+      })
   }
 })
