698fc3e954bfadab974303d78bc2f159e974827b
nasen23 <1439468790@qq.com>
2019-10-27 17:22:01 +0800

    feat: center avatar and calculate padding

--- frontend/pages/mainpage/mainpage.js
+++ frontend/pages/mainpage/mainpage.js
@@ -1,22 +1,24 @@
 Page({
   data: {
-    historyActivities: ["历史1", "历史2"],
-    activityTypes: ["全部", "类型1", "类型2"],
+    historyActivities: ['历史1', '历史2'],
+    activityTypes: ['全部', '类型1', '类型2'],
     activities: [
-      { "id": 1, "name": "活动名称", "intro": "活动简介", "tags": ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"], "activityTime": "2019年10月14日", "activityLocation": "清华大学桃李园地下餐厅", "activityObject": ["xxxx级本科/研究生", "xx专业/院系","年龄段"],},
-      { "id": 2, "name": "活动名称", "intro": "活动简介", "tags": ["1"] },
-      { "id": 3, "name": "活动名称", "intro": "活动简介", "tags": ["1"] },
-      { "id": 4, "name": "活动名称", "intro": "活动简介", "tags": ["1"] },
-      { "id": 5, "name": "活动名称", "intro": "活动简介", "tags": ["1"] }
+      { 'id': 1, 'name': '活动名称', 'intro': '活动简介', 'tags': ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'], 'activityTime': '2019年10月14日', 'activityLocation': '清华大学桃李园地下餐厅', 'activityObject': ['xxxx级本科/研究生', 'xx专业/院系','年龄段'],},
+      { 'id': 2, 'name': '活动名称', 'intro': '活动简介', 'tags': ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'], 'activityTime': '2019年10月14日', 'activityLocation': '清华大学桃李园地下餐厅', 'activityObject': ['xxxx级本科/研究生', 'xx专业/院系','年龄段'],},
+      { 'id': 3, 'name': '活动名称', 'intro': '活动简介', 'tags': ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'], 'activityTime': '2019年10月14日', 'activityLocation': '清华大学桃李园地下餐厅', 'activityObject': ['xxxx级本科/研究生', 'xx专业/院系','年龄段'],},
+      { 'id': 4, 'name': '活动名称', 'intro': '活动简介', 'tags': ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'], 'activityTime': '2019年10月14日', 'activityLocation': '清华大学桃李园地下餐厅', 'activityObject': ['xxxx级本科/研究生', 'xx专业/院系','年龄段'],},
+      { 'id': 5, 'name': '活动名称', 'intro': '活动简介', 'tags': ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'], 'activityTime': '2019年10月14日', 'activityLocation': '清华大学桃李园地下餐厅', 'activityObject': ['xxxx级本科/研究生', 'xx专业/院系','年龄段'],},
+      { 'id': 6, 'name': '活动名称', 'intro': '活动简介', 'tags': ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'], 'activityTime': '2019年10月14日', 'activityLocation': '清华大学桃李园地下餐厅', 'activityObject': ['xxxx级本科/研究生', 'xx专业/院系','年龄段'],},
+      { 'id': 7, 'name': '活动名称', 'intro': '活动简介', 'tags': ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'], 'activityTime': '2019年10月14日', 'activityLocation': '清华大学桃李园地下餐厅', 'activityObject': ['xxxx级本科/研究生', 'xx专业/院系','年龄段'],},
+      { 'id': 8, 'name': '活动名称', 'intro': '活动简介', 'tags': ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'], 'activityTime': '2019年10月14日', 'activityLocation': '清华大学桃李园地下餐厅', 'activityObject': ['xxxx级本科/研究生', 'xx专业/院系','年龄段'],},
+      { 'id': 9, 'name': '活动名称', 'intro': '活动简介', 'tags': ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'], 'activityTime': '2019年10月14日', 'activityLocation': '清华大学桃李园地下餐厅', 'activityObject': ['xxxx级本科/研究生', 'xx专业/院系','年龄段'],},
+      { 'id': 10, 'name': '活动名称', 'intro': '活动简介', 'tags': ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'], 'activityTime': '2019年10月14日', 'activityLocation': '清华大学桃李园地下餐厅', 'activityObject': ['xxxx级本科/研究生', 'xx专业/院系','年龄段'],},
+      { 'id': 11, 'name': '活动名称', 'intro': '活动简介', 'tags': ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'], 'activityTime': '2019年10月14日', 'activityLocation': '清华大学桃李园地下餐厅', 'activityObject': ['xxxx级本科/研究生', 'xx专业/院系','年龄段'],},
     ]
-  },
-  inputTyping: function () {
-    var obj = JSON.stringify(this.data.history);
-    wx.navigateTo({url:"./searchpage/search?history=" + obj})
   },
   toActivityDetail: function (e) {
     wx.navigateTo({
-      url: "/pages/activity/activity-detail/index?id=" + e.currentTarget.dataset.id
+      url: '/pages/activity/activity-detail/index?id=' + e.currentTarget.dataset.id
     })
   }
 })
