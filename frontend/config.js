ddb3fc8fede1493b8cf8ab016a0157fc5ed6efcd
QiuYuanhui <qiuyh0924@163.com>
2019-12-01 17:45:33 +0800

    feat: get all activities from backend and display (only id, name and intro fields)

--- frontend/config.js
+++ frontend/config.js
@@ -1,6 +1,4 @@
 module.exports = {
   version: "0.0.1",
-  note: '',
-  subDomain: "tz", // https://admin.it120.cc 登录后台首页的专属域名
-  requireBindMobile: true, // 是否强制绑定手机号码才能使用
+    host: "http://192.168.0.104:3000/"
 }
