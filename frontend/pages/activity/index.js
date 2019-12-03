6994cca0372e514d5ad50716b890f5589439371d
QiuYuanhui <qiuyh0924@163.com>
2019-12-03 15:21:25 +0800

    feat: add activity page

---
+++ frontend/pages/activity/index.js
@@ -0,0 +1,11 @@
+Page({
+    data:{
+        activities:[
+            {
+                title: "七字班软学院聚餐",
+                countPerson: 20,
+                tags: ["七字班","聚餐"]
+            }
+        ]
+    }
+})
