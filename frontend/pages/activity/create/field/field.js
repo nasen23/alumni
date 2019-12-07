0d9e032e58ffd6c91dd61870f9904e5b0ed48dad
QiuYuanhui <qiuyh0924@163.com>
2019-12-07 18:15:49 +0800

    feat: add field description (placeholder)

--- frontend/pages/activity/create/field/field.js
+++ frontend/pages/activity/create/field/field.js
@@ -1,9 +1,15 @@
 Page({
   data: {
     name: "",
+    desc: "",
     show: false,
+    field: {
+      name: "",
+      type: "",
+      desc: "",
+      chosen: false,
+    },
     type: "singleLineText",
-    intro: "",
   },
 
   onLoad (options) {
@@ -15,27 +21,64 @@ Page({
   },
 
   onNameFilledIn (e) {
-
+    this.setData({ name: e.detail })
   },
 
-  onIntroFilledIn (e) {
-
+  onDescFilledIn (e) {
+    this.setData({ desc: e.detail })
   },
 
   onTypeFilledIn (e) {
     this.setData({ show: true })
   },
 
-  onMinCharCntFilledIn (e) {
-
+  onTagClicked (e) {
+    this.setData({
+      show: false,
+      type: e.currentTarget.dataset.type,
+    })
   },
 
-  onMaxCharCntFilledIn (e) {
-
+  onOverlayClicked () {
+    this.setData({ show: false })
   },
 
   onSave () {
+    const this_ = this
+         
+    if (!this.data.name) {
+      wx.showModal({
+        title: "提示",
+        content: "请填写字段名称",
+        showCancel: false
+      })
+      return
+    }
 
+    this.setData({
+      field: {
+        chosen: false,
+        name: this.data.name,
+        type: this.data.type,
+        desc: this.data.desc,
+      }
+    })
+
+    wx.showModal({
+      title: "提示",
+      content: "添加成功",
+      showCancel: false,
+      success (res) {
+        let pages = getCurrentPages()
+        // Previous page
+        let prevPage = pages[pages.length - 2]
+
+        prevPage.setData({
+          allFields: prevPage.data.allFields.concat(this_.data.field)
+        })
+        wx.navigateBack()
+      }
+    })
   },
 
 })
