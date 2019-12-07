a1111cf3d4d44f494fe2542e26e613d88911214f
QiuYuanhui <qiuyh0924@163.com>
2019-12-07 14:42:19 +0800

    feat: add self-defined field page

---
+++ frontend/pages/activity/create/field/field.js
@@ -0,0 +1,41 @@
+Page({
+  data: {
+    name: "",
+    show: false,
+    type: "singleLineText",
+    intro: "",
+  },
+
+  onLoad (options) {
+
+  },
+
+  onShow () {
+
+  },
+
+  onNameFilledIn (e) {
+
+  },
+
+  onIntroFilledIn (e) {
+
+  },
+
+  onTypeFilledIn (e) {
+    this.setData({ show: true })
+  },
+
+  onMinCharCntFilledIn (e) {
+
+  },
+
+  onMaxCharCntFilledIn (e) {
+
+  },
+
+  onSave () {
+
+  },
+
+})
