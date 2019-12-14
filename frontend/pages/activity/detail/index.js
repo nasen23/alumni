b4fc3c43b995ade2c6dcc61f9a5ee8756ec2ebc2
QiuYuanhui <qiuyh0924@163.com>
2019-12-14 23:25:49 +0800

    feat: activity detail page improved
    add check of administratrative authority;
    add entrance of changing registration information;
    add entrance of canceling registration.

--- frontend/pages/activity/detail/index.js
+++ frontend/pages/activity/detail/index.js
@@ -8,8 +8,11 @@ Page({
     popupShow: false,
   },
 
-  onLoad: function (e) {
-    this.setData({ id: e.id })
+  onLoad (e) {
+    this.setData({
+      id: e.id,
+      user: app.globalData.openid
+    })
   },
 
   onShow () {
@@ -20,7 +23,7 @@ Page({
       data: {
         id: this_.data.id
       },
-      success: res => {
+      success (res) {
         let actStart = res.data.actStart
         let actEnd = res.data.actEnd
         this_.setData({
@@ -32,14 +35,23 @@ Page({
           phone: res.data.phone,
           pictures: res.data.pictures,
           organizer: res.data.organizer,
+          administrators: res.data.administrators,
           participants: res.data.participants,
           maxParticipants: res.data.maxParticipants,
           actStart: actStart === "0" ? "0" :
-            new Date(parseInt(actStart)).toLocaleString(),
+            new Date(parseInt(actStart)).toLocaleString('zh-CN'),
           actEnd: actEnd === "0" ? "0" :
-            new Date(parseInt(actEnd)).toLocaleString(),
-          signupStart: new Date(parseInt(res.data.signupStart)).toLocaleString('zh'),
-          signupEnd: new Date(parseInt(res.data.signupEnd)).toLocaleString('zh'),
+            new Date(parseInt(actEnd)).toLocaleString('zh-CN'),
+          signupStart: new Date(parseInt(res.data.signupStart)).toLocaleString('zh-CN'),
+          signupEnd: new Date(parseInt(res.data.signupEnd)).toLocaleString('zh-CN'),
+          signinCode: res.data.signinCode
+        })
+
+        let ret = this_.isParticipant()
+        this_.setData({
+          isAdministrator: this_.isAdministrator(),
+          isParticipant: ret.isIn,
+          index: ret.index
         })
 
         wx.request({
@@ -56,19 +68,59 @@ Page({
           }
         })
       },
-      fail: e => {
-
-      }
+      fail(){}
     })
 
   },
 
-  toActivitySignup: function() {
-    wx.navigateTo({ url: '/pages/activity/signup/index?id=' + this.data.id })
+  isAdministrator () {
+    return this.data.administrators.includes(this.data.user)
   },
 
-  toActivityManage (e) {
+  isParticipant () {
+    let pars = this.data.participants
+    if (pars.length === 0) {
+      return {
+        index: -1,
+        isIn: false
+      }
+    }
+
+    for (let [index, par] of Object.entries(pars)) {
+      if (par.openid === this.data.user) {
+        return {
+          index,
+          isIn: true
+        }
+      }
+    }
+
+    return {
+      index: -1,
+      isIn: false
+    }
+  },
+
+  toActivitySignup () {
+    wx.navigateTo({
+      url: `../signup/index?id=${this.data.id}`
+    })
+  },
+
+  toActivityManage () {
     this.setData({ popupShow: true })
+  },
+
+  changeSignupInfo () {
+    const this_ = this
+    wx.navigateTo({
+      url: `../signup/index?id=${this.data.id}`,
+      success (res) {
+        res.eventChannel.emit('changeInfo', {
+          data: this_.data.participants[this_.data.index]
+        })
+      }
+    })
   },
 
   onOverlayClicked () {
@@ -109,7 +161,7 @@ Page({
         fail (error) {
           wx.showModal({
             title: "提示",
-            content: "删除活动失败，请检查网络状态",
+            content: "删除活动失败",
             showCancel: false,
             success (res) {
               wx.navigateBack()
@@ -125,7 +177,12 @@ Page({
   },
 
   tagSignInClicked () {
+    let this_ = this
+
     this.setData({ popupShow: false })
+    wx.navigateTo({
+      url: `../signin/signin?id=${this_.data.id}&code=${this_.data.signinCode}`
+    })
   },
 
   tagStatisticClicked () {
