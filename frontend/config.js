1dc52efae93e285f120e079019bf489bff19277f
QiuYuanhui <qiuyh0924@163.com>
2019-12-16 15:02:30 +0800

    refactor: config.js, util.js
    integrate all back-end routing address into config.js
    
    integrate all wx.request into util.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,8 +1,36 @@
 module.exports = {
   version: "0.0.1",
- // host: "http://localhost:3389/",
- // host: "http://2019-a03.iterator-traits.com:3389/",
-  host: "http://192.168.0.104:3389/",
+  host: "http://localhost:3389/",
+  // host: "http://2019-a03.iterator-traits.com:3389/",
+  // host: "http://192.168.0.104:3389/",
+  routes: {
+    getAllActs: this.host + 'activity/all',
+    getSingAct: this.host + 'activity/get',
+    postAddAct: this.host + 'activity/add',
+    delSingAct: this.host + 'activity/delete',
+    delAllActs: this.host + 'activity/delete/all',
+    postAddPic: this.host + 'activity/upload-picture',
+
+    postAllActs: this.host + 'activity/all',
+    getUserActs: this.host + 'activity/user',
+
+    putUpdataAct: this.host + 'activity/put',
+    getSigninAct: this.host + 'activity/signin',
+
+    postSignupAct: this.host + 'activity/signup',
+
+    getCheckSignin: this.host + 'activity/check-signin',
+
+    putUpdateSignup: this.host + 'activity/signup',
+    delCancelSignup: this.host + 'activity/signup',
+
+    getAllUser: this.host + 'user/all',
+    getSingUser: this.host + 'user/get',
+    postAuthUser: this.host + 'user/auth',
+    postLoginUser: this.host + 'user/login',
+    putUpdateUser: this.host + 'user/put',
+  },
+
   appId: "wx97a987f625c04b01",
   appSecret: "c0121d2e27bc2c1e1f394c83b503d4e6",
   signupSuccessMsgId: "Cs1N92eQh3MxC5lx-i7jR_-Ru0f4YhOuchF7VNjRlVA",
--- frontend/utils/util.js
+++ frontend/utils/util.js
@@ -1,19 +1,19 @@
-const formatTime = date => {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
-  const hour = date.getHours()
-  const minute = date.getMinutes()
-  const second = date.getSeconds()
-
-  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
-}
-
-const formatNumber = n => {
-  n = n.toString()
-  return n[1] ? n : '0' + n
+const request = (url, method, data) => {
+  return new Promise(function (resolve, reject) {
+    wx.request({
+      url,
+      method,
+      data,
+      success (res) {
+        resolve(res.data)
+      },
+      fail (err) {
+        reject(err)
+      }
+    })
+  })
 }
 
 module.exports = {
-  formatTime: formatTime
+  request
 }
