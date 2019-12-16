98dfd5da676978e536e1f94f1a8a8bfe9b292444
QiuYuanhui <qiuyh0924@163.com>
2019-12-16 15:02:05 +0800

    feat: finish search page

--- frontend/pages/mainpage/searchpage/search.js
+++ frontend/pages/mainpage/searchpage/search.js
@@ -1,15 +1,59 @@
+const app = getApp()
+const config = require('../../../config')
+
 Page({
-  data: {},
+  data: {
+    value: "",
+    activities: [],
+    rootPath: config.host
+  },
 
   onLoad (options) {
 
   },
 
-  onSearch () {
+  onChange (e) {
+    this.setData({
+      value: e.detail
+    })
+  },
 
+  onSearch () {
+    let this_ = this
+
+    if (!this.data.value) {
+      wx.showModal({
+        title: "提示",
+        content: "请输入关键词",
+        showCancel: false
+      })
+      return
+    }
+
+    wx.request({
+      url: config.host + 'activity/all?name=' + this.data.value,
+      method: "POST",
+      data: {
+        fields: ["id", "name", "intro", "pictures"]
+      },
+      success (res) {
+        this_.setData({
+          activities: res.data
+        })
+      },
+      fail (e) {
+        console.log(e)
+      }
+    })
   },
 
   onCancel () {
 
+  },
+
+  toActivityDetail (e) {
+    wx.navigateTo({
+      url: '../../activity/detail/index?id=' + e.currentTarget.dataset.id
+    })
   }
 });
